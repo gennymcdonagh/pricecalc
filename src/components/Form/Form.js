c4261aeb7b32ce8a878926ba1f44463c57829c65
GennyMcDonagh <gennymcdonagh@gmail.com>
2019-02-16 11:36:15 +1100

    useState hook in form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,77 +6,61 @@ units
 write test for input
 */
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { NumberInput } from '../Input/NumberInput';
 import './Form.css';
 import PropTypes from 'prop-types';
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cost: "",
-      items: "",
-      time: "",
-      wage: "",
-      markup: ""
-    };
+export const Form = (props) => {
+  const [cost, setCost] = useState("");
+  const [items, setItems] = useState("");
+  const [time, setTime] = useState("");
+  const [wage, setWage] = useState("");
+  const [markup, setMarkup] = useState("");
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+  return (
+    <div className="form">
+      <NumberInput
+        value={cost}
+        onChange={e => setCost(e.target.value)}
+        label="Cost of materials"
+        id="cost"
+      />
 
-  handleInputChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+      <NumberInput
+        value={items}
+        onChange={e => setItems(e.target.value)}
+        label="Number of items made"
+        id="items"
+      />
 
-  render() {
-    const { calculateTotals } = this.props;
-    const { cost, items, time, wage, markup } = this.state;
+      <NumberInput
+        value={time}
+        onChange={e => setTime(e.target.value)}
+        label="Hours to make that many items"
+        id="time"
+      />
 
-    return (
-      <div className="form">
+      <NumberInput
+        value={wage}
+        onChange={e => setWage(e.target.value)}
+        label="Hourly wage"
+        id="wage"
+      />
 
-        <NumberInput
-          value={cost}
-          onChange={this.handleInputChange}
-          label="Cost of materials"
-          id="cost"
-        />
+      <NumberInput
+        value={markup}
+        onChange={e => setMarkup(e.target.value)}
+        label="Retail profit markup %"
+        id="markup"
+      />
 
-        <NumberInput
-          value={items}
-          onChange={this.handleInputChange}
-          label="Number of items made"
-          id="items"
-        />
-
-        <NumberInput
-          value={time}
-          onChange={this.handleInputChange}
-          label="Hours to make that many items"
-          id="time"
-        />
-
-        <NumberInput
-          value={wage}
-          onChange={this.handleInputChange}
-          label="Hourly wage"
-          id="wage"
-        />
-
-        <NumberInput
-          value={markup}
-          onChange={this.handleInputChange}
-          label="Retail profit markup %"
-          id="markup"
-        />
-
-        <button onClick={() => calculateTotals(this.state)}>Calculate</button>
-      </div>
-    );
-  }
+      <button 
+        onClick={() => props.calculateTotals({cost, items, time, wage, markup})}>
+        Calculate
+      </button>
+    </div>
+  )
 }
 
 Form.propTypes = {
