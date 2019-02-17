b9c34c387252f3da291a40bf4acf8c4d51a7d994
GennyMcDonagh <gennymcdonagh@gmail.com>
2019-02-17 21:07:23 +1100

    add ghpages build

---
+++ docs/precache-manifest.ddaa003e15f0192c614b6baaeb02df8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03ecc4417d2bb1facef9",
+    "url": "/pricecalc/static/js/runtime~main.03ecc441.js"
+  },
+  {
+    "revision": "47d5c7d8e237668aadc6",
+    "url": "/pricecalc/static/js/main.47d5c7d8.chunk.js"
+  },
+  {
+    "revision": "7f7ff998227c62abd55e",
+    "url": "/pricecalc/static/js/2.7f7ff998.chunk.js"
+  },
+  {
+    "revision": "47d5c7d8e237668aadc6",
+    "url": "/pricecalc/static/css/main.55006238.chunk.css"
+  },
+  {
+    "revision": "7ab5f77a3d72f8ce9d7a84e4eba4294f",
+    "url": "/pricecalc/index.html"
+  }
+];
